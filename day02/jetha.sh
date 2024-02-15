a4e197569859550dbb441de66f9d7969c738a160
Ubuntu <ubuntu@ip-172-31-43-32.ap-south-1.compute.internal>
2024-02-15 08:04:02 +0000

    elif conditon added

--- day02/jetha.sh
+++ day02/jetha.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
- 
+
 #read -p "jethalal ne mukde kisko dekha:"$bandi
+read -p "jethalal loves babita:" babita
 
 if [[ $1 == "daya bhabhi" ]]; then
     echo "jetha is loyal"
+elif [[ $babita == "babita" ]]; then
+    echo "jetha is still loyal"
 else
-    echo "jetha is not loyal" 
+    echo "jetha is not loyal"
 fi
+
 
