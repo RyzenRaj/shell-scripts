fc63fea8e62833e2c2d118332e4ed89861a9d6c3
Ubuntu <ubuntu@ip-172-31-43-32.ap-south-1.compute.internal>
2024-02-15 07:56:41 +0000

    learned if else and arguments

---
+++ day02/create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read  -p "enter the username:" username
+name = username
+echo "user is $username"
+echo "you entered"
+echo "new argument  added $1"
---
+++ day02/jetha.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+#read -p "jethalal ne mukde kisko dekha:"$bandi
+
+if [[ $1 == "daya bhabhi" ]]; then
+    echo "jetha is loyal"
+else
+    echo "jetha is not loyal" 
+fi
+
